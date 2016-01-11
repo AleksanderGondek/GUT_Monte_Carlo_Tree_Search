22f62e962cee2b6f276ce081ecb6e80defebe644
Aleksander Gondek <gondekaleksander@hotmail.com>
2016-01-11 17:53:52 +0100

    Further ammandments to build_and_run

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DESTINATION_XEON_PHI="mic0"
+
 echo "~~~ GUT Monte-Carlo Tree Search Project ~~~"
 echo "Build and run script starting up.."
 
@@ -44,8 +46,36 @@ if [ ${environment} = "gut" ]; then
         exit 1
     fi
 
+    echo "Coping needed libs to mic machine.."
+    scp /opt/intel/composer_xe_2013_sp1.3.174/compiler/lib/mic/libmiomp5.so ${DESTINATION_XEON_PHI}:~
+    if [ $? -ne 0 ]; then
+        echo "There was an error in coping needed libs to mic machine"
+        exit 1
+    fi
+
+    echo "Coping compiled application to mic machine.."
+    scp .bin/GUT_Monte_Carlo_Tree_Search ${DESTINATION_XEON_PHI}:~
+    if [ $? -ne 0 ]; then
+        echo "There was an error in coping compiled application to mic machine"
+        exit 1
+    fi
+
+    echo "SSH to mic machine"
+    ssh ${DESTINATION_XEON_PHI}
+    if [ $? -ne 0 ]; then
+        echo "There was an error in SSHing to mic machine"
+        exit 1
+    fi
+
+    echo "Exporting library path"
+    export LD_LIBRARY_PATH="~"
+    if [ $? -ne 0 ]; then
+        echo "There was an error in exporting library path"
+        exit 1
+    fi
+
     echo "Starting up.."
-    ./bin/GUT_Monte_Carlo_Tree_Search
+    ~/GUT_Monte_Carlo_Tree_Search
     if [ $? -ne 0 ]; then
         echo "GUT_Monte_Carlo_Tree_Search exited with non-zero status"
         exit 1
@@ -53,3 +83,5 @@ if [ ${environment} = "gut" ]; then
 else
     echo "Not yet implemented."
 fi
+
+exit 0
