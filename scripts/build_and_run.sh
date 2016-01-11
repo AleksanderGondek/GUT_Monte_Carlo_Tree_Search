b095fddd33c5c2907c52935e753a1d95bd3fae3b
Aleksander Gondek <gondekaleksander@hotmail.com>
2016-01-11 15:15:05 +0100

    Working towards runnable openMP hello world

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,1 +1,31 @@
 #!/usr/bin/env bash
+
+echo "~~~ GUT Monte-Carlo Tree Search Project ~~~"
+echo "Build and run script starting up.."
+
+echo "Making sure the script was run properly.."
+if [ ! -f "CMakeLists.txt" ]; then
+    echo "Build & Run script was not run from project root directory!"
+    echo "Please do run it from root."
+    exit 1
+fi
+
+echo "Removing remnants of previous builds and runs.."
+if [ -d "bin" ]; then
+    echo "Removing .bin/*"
+    rm -rf "./bin"
+fi
+if [ -d "lib" ]; then
+    echo "Removing .lib/*"
+    rm -rf "./lib"
+fi
+
+echo "Detecting run environment.."
+current_user="$(id -u -n)"
+if [ ${current_user} = "agondek" ]; then
+    echo "Environment set to dev"
+    environment="dev"
+else
+    echo "Environment set to gut"
+    environment="gut"
+fi
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main()
+{
+    char const* str = "Hello world";
+    unsigned const len = std::strlen(str);
+#pragma omp parallel for num_threads(4)
+    for (unsigned thread = 0; thread < 4; ++thread)
+    {
+        for (unsigned i = 0; i < len; ++i)
+        {
+            std::cout << str[i] << std::endl;
+        }
+    }
 }
